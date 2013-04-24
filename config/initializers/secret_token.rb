b2839dcf0bacff4151a6547f3ad801bdb191b473
Nathan Brakken <nathanbrakken@EVAITMAC.local>
2013-04-23 21:52:29 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blerg::Application.config.secret_token = '0040087d69f722c8d91e0f1fbfbcec02f845a49a5afc2160120b23fa06a0e103cc10f01530f3ba1f4d93ce51fa65aa4d8443dd8ffaf3398717b5e34c747b2e5f'
